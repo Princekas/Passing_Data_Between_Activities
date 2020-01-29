51c6bba91f88baa4ddfc97aa9160afde45c991d7
Prince99-A <55297392+Prince99-A@users.noreply.github.com>
2020-01-29 21:23:11 +0530

    Initial commit

---
+++ app/src/main/java/com/prince/passing_data_between_activities/MainActivity.java
@@ -0,0 +1,16 @@
+//To pass dat aBetween Activities we use Bundles.
+
+package com.prince.passing_data_between_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
